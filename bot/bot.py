fb8232656abe0860ba63f4e9daef9965ac6c4bfe
Lazaro Raul <riglesiasvera@gmail.com>
2020-10-20 13:34:36 -0400

    [bot] refs #13 - Method for set the bot commands description

---
+++ bot/bot.py
@@ -0,0 +1,9 @@
+
+def set_commands(bot):
+    bot.set_my_commands([
+        ('create'   , 'New schedule discuss'),
+        ('config'   , 'Set the schedule options'),
+        ('vote'     , 'Take part in the current discussion'),
+        ('close'    , 'Close the current discussion'),
+        ('cancel'   , 'Cancel the current action'),
+    ])
