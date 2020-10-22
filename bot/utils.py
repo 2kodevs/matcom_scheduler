bbaf3a47548467457cc048d234afd64217a11e44
cbermudez <cbermudez.dev@gmail.com>
2020-10-21 20:30:08 -0400

    [bot] refs #6 - Implement helper methods for vote command

--- bot/utils.py
+++ bot/utils.py
@@ -7,3 +7,19 @@ def is_chat_admin(bot, chat_id, user_id):
         if member.user.id == user_id:
             return True
     return False
+
+def get_or_init(d: dict, key, default):
+    result = None
+    try:
+        result = d[key]
+    except KeyError:
+        result = d[key] = default
+    return result
+
+def clean_vote_data(data):
+    try:
+        del data['source_id']
+        del data['options']
+        del data['selected']
+    except KeyError:
+        pass
