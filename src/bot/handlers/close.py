9d55e89bca5cd2bad06c286e88a6019034f08b3a
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-08 18:31:19 -0500

    [bot][model] refs #30 - If no model name is provided to use_model,  default_model is used

--- src/bot/handlers/close.py
+++ src/bot/handlers/close.py
@@ -17,8 +17,8 @@ def close(update, context):
         assert any(context.chat_data.get('voters', dict()).values()), NO_VOTES
 
         votes = [v for v in context.chat_data['voters'].values() if v]
-        model = context.chat_data.get('model', 'positions')
-        solution = use_model(model, votes)
+        model = context.chat_data.get('model')
+        solution = use_model(votes, model)
         sol_msg = 'Los resultados de la votación son: \n'
         sol_msg += enumerate_options(solution)
         context.bot.send_message(chat_id=chat, text=sol_msg)
--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,15 +1,20 @@
 from .base import ModelError
 from .model import Positions
 
-def use_model(model_name, data):
+def use_model(data, model_name=None):
     '''
     Find a model with name `model_name` and 
     return the solution of eval data
     '''
+    if model_name is None:
+        model_name = defaul_model
     for model in models:
         if model.__name__.lower() == model_name.lower():
             return model().solve(data)
     raise ModelError('Model not found')
+
+
+defaul_model = Positions.__name__
 
 models = [
     Positions,
