9cd7b5e4d2d76ce4604e02c214990f2c7954f8b0
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-08 15:32:00 -0500

    [model] - Public list of available models and method for search by name
    Changes:
    - `models`: list of model implementations
    - `use_model`: method to find and use a specific model

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,1 +1,16 @@
-from .model import solve
+from .base import ModelError
+from .model import Positions
+
+def use_model(model_name, data):
+    '''
+    Find a model with name `model_name` and 
+    return the solution of eval data
+    '''
+    for model in models:
+        if model.__name__.lower() == model_name.lower():
+            return model().solve(data)
+    raise ModelError('Model not found')
+
+models = [
+    Positions,
+]
