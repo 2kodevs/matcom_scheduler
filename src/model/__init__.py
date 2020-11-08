f4e78256247613694043aaa953b0b2d36b54b665
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-08 18:34:47 -0500

    [model] refs #30 - Fix a typo

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -7,14 +7,14 @@ def use_model(data, model_name=None):
     return the solution of eval data
     '''
     if model_name is None:
-        model_name = defaul_model
+        model_name = default_model
     for model in models:
         if model.__name__.lower() == model_name.lower():
             return model().solve(data)
     raise ModelError('Model not found')
 
 
-defaul_model = Positions.__name__
+default_model = Positions.__name__
 
 models = [
     Positions,
