ee65e345ceba60ce0dbf50fe38e3972b719619c5
Lazaro Raul <47638426+stdevRulo@users.noreply.github.com>
2020-11-09 15:08:04 -0500

    Update src/model/minumun.py
    Co-authored-by: Miguel Tenorio <46824157+stdevAntiD2ta@users.noreply.github.com>

--- src/model/minumun.py
+++ src/model/minumun.py
@@ -1,7 +1,7 @@
 from .model import Positions
 
 class Minimum(Positions):
-    '''Busca el calendario que mínimice la máxima cantidad de cambios de orden con respecto a las propuestas'''
+    '''Busca el calendario que minimice la máxima cantidad de cambios de orden con respecto a las propuestas'''
     
     def merge(self, d1, d2):
         return max(d1, d2)
