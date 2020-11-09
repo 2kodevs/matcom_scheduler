55447ad651d93baa384edbfe9debf7a7fd43b4a6
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-09 14:29:08 -0500

    [model] refs #30 - Model to select the more popular calendar

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,6 +1,7 @@
 from .base import ModelError
 from .model import Positions
 from .minumun import Minimum
+from .more_popular import MorePopular
 
 def use_model(data, model_name=None):
     '''
@@ -20,4 +21,5 @@ default_model = Positions.__name__
 models = [
     Positions,
     Minimum,
+    MorePopular,
 ]
--- src/model/minumun.py
+++ src/model/minumun.py
@@ -2,5 +2,6 @@ from .model import Positions
 
 class Minimum(Positions):
     '''Busca el calendario que mínimice la máxima cantidad de cambios de orden con respecto a las propuestas'''
+    
     def merge(self, d1, d2):
         return max(d1, d2)
