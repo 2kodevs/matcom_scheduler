eb5b7a690e96a00a29cb6c253234a15f365c09f4
Lazaro Raul <riglesiasvera@gmail.com>
2020-11-23 20:05:23 -0500

    [model] - DEA/AR e value updated
    Also some error fixed

--- src/model/dea_ar_model.py
+++ src/model/dea_ar_model.py
@@ -6,11 +6,14 @@ from scipy.optimize import linprog
 class DEA_AR(BaseModel):
     '''Busca el calendario más favorable según el modelo de exclusión DEA/AR(Data Envelopment Analysis/Assurance Region)'''
 
-    def __init__(self, e=0.0001): # //TODO: select the correct default value
+    def __init__(self, e=1.056e-07):
         super().__init__()
         self.e = e
     
     def solve(self, data:list):
+        if not data:
+            return None
+
         t = len(data[0])
         yrj = {element:[0]*t for element in data[0]}
 
@@ -23,10 +26,10 @@ class DEA_AR(BaseModel):
         ceros = [0]*t
         matrix, values = [], []
         for r in range(t - 1):
-            matrix.append([*ceros[:r], -1, 1, *ceros[:t-(r+1)]])
+            matrix.append([*ceros[:r], -1, 1, *ceros[:t-(r+2)]])
             values.append(-self.e)
         for r in range(t - 2):
-            matrix.append([*ceros[:r], -1, 2, -1, *ceros[:t-(r+2)]])
+            matrix.append([*ceros[:r], -1, 2, -1, *ceros[:t-(r+3)]])
             values.append(0)
         matrix.append(ceros)
         matrix[-1][-1] = -1
