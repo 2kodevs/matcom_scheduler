75fba161e156b63362ea03afad7c25bfceb4ddaa
Carlos Bermudez <cbermudez.dev@gmail.com>
2021-10-24 21:01:27 -0400

    Show partial results in status message

--- src/bot/handlers/status.py
+++ src/bot/handlers/status.py
@@ -1,23 +1,34 @@
 import re
 
+from ...model import use_model
 from .filters import private_text_filter
+from .utils import enumerate_options, is_chat_admin
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from telegram.ext import CommandHandler, CallbackQueryHandler, Filters, MessageHandler
 
 
+ADMINS_ONLY     = 'Ups!!!, solo los administradores pueden usar este comando :('
 NO_ACTIVE = 'No hay una discusión activa en este grupo.'
 NO_CONFIG = 'No se ha configurado completamente aún la discusión actual.'
 NO_VOTERS = 'La discusión activa en este grupo no tiene votantes aún.'
-LIST      = 'En estos momentos han votado: %s personas.'
+LIST      = 'En estos momentos han votado: %s personas.\n\n'
 
 def status_group_voters(update, context):
+    user = update.effective_user.id
+    chat = update.effective_chat.id
     try:
+        assert is_chat_admin(context.bot, chat, user), ADMINS_ONLY
         assert context.chat_data.get('active'), NO_ACTIVE
         assert context.chat_data.get('options'), NO_CONFIG
         assert any(context.chat_data.get('voters', dict()).values()), NO_VOTERS
         voters: dict = context.chat_data['voters']
         cant = sum([1 for _, vote in voters.items() if vote])
-        assert False, LIST%cant
+        votes = [v for v in context.chat_data['voters'].values() if v]
+        model = context.chat_data.get('model')
+        solution = use_model(votes, model)
+        sol_msg = 'Los resultados parciales de la votación son: \n'
+        sol_msg += enumerate_options(solution)
+        assert False, (LIST%cant) + sol_msg
     except AssertionError as e:
         update.effective_message.reply_text(str(e))
 
